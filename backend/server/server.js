480428107e5da2fbe209a50530fea0ccadee9daa
SebasRusuu <20220905@iade.pt>
2025-03-28 19:51:34 +0000

    Backend adicionado

---
+++ backend/server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 8080;
+app.use(cors());
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:8080`);
+});
