30e629fc25757977bd06608d0e40cebd2e5f9dbc
Ze Franco <josepedromartinspfranco@gmail.com>
2025-03-29 14:09:28 +0000

    Página de perfil criado

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,11 @@
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
-import Signup from './pages/signup/signup'
 import CreateProjectPage from './pages/project_page/project'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from './pages/Signup/Signup';
+import Perfil from './pages/Perfil/Perfil';
 
 const App = () => {
   return (
@@ -9,10 +13,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Signup />} />
         <Route path="/project" element={<CreateProjectPage />} />
+        <Route path="/perfil" element={<Perfil />} />
       </Routes>
     </Router>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
